13210edf6ff0461544a04510d822540264cec4a2
Vo Trong Dan <trongdantvh@gmail.com>
2023-11-03 16:27:49 +0700

    update class Dictionary

--- dictionary/src/main/java/dictionary/Dictionary.java
+++ dictionary/src/main/java/dictionary/Dictionary.java
@@ -4,4 +4,12 @@ import java.util.ArrayList;
 
 public class Dictionary {
     private ArrayList<String> dictionary = new ArrayList<String>();
+
+    public void setDictionary(ArrayList<String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public ArrayList<String> getDictionary() {
+        return dictionary;
+    }
 }
