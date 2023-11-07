a62c999abcc4a0100157bc97acd36252716a481b
Vo Trong Dan <trongdantvh@gmail.com>
2023-11-08 00:04:14 +0700

    update class DictionaryCommandline

--- dictionary/src/main/java/dictionary/DictionaryCommandline.java
+++ dictionary/src/main/java/dictionary/DictionaryCommandline.java
@@ -1,60 +1,63 @@
 package dictionary;
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Dictionary;
 import java.util.Scanner;
 
 public class DictionaryCommandline {
-  public void showAllWords(Dictionary dictionary){
-        // sắp xếp các từ theo alphabet
-        Collections.sort(dictionary.getWords(), new Comparator<Word>()) {
-            public int compare(Word no1, Word no2) {
-                return no1.getEnglish().compareTo(no2.getEnglish());
-            }
-        };
-        System.out.println("No | English | Vietnamese");
-        for(int i = 0; i < dictionary.getWords().size(); i++) {
-            Word word = dictionary.getWords().get(i);
-            System.out.println((i+1) + " | " + word.getEnglish() + " | " + word.getVietnamese());
-        }
-    }
+//   public void showAllWords(Dictionary dictionary) throws IOException {
+//         // sắp xếp các từ theo alphabet
+//         ArrayList<Word> words = dictionary.getWords();
+//         Collections.sort(dictionary.getWords(), new Comparator<Word>()) {
+//             public int compare(Word no1, Word no2) {
+//                 return no1.getWord_target().compareTo(no2.getWord_target());
+//             }
+//         };
+//         System.out.println("No | English | Vietnamese");
+//         for(int i = 0; i < dictionary.getWords().size(); i++) {
+//             Word word = dictionary.getWords().get(i);
+//             System.out.println((i+1) + " | " + word.getWord_target() + " | " + word.getWord_explain());
+//         }
+//     }
 
-    public void insertFromCommandline(Dictionary dictionary) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập số lượng từ vựng muốn thêm: ");
-        int n = sc.nextInt();
-        sc.nextLine();
-        for(int i = 0; i < n ;i ++) {
-            System.out.println("Nhập từ vựng tiếng anh: ");
-            String english = sc.nextLine();
-            System.out.println("Nhập nghĩa tiếng việt: ");
-            String vietnamese = sc.nextLine();
-            Word word = new Word(english, vietnamese); //tạo đối tượng word
-            dictionary.addWord(word); // thêm word vào dictionary
-        }
-    }
-    public void dictionaryBasic(Dictionary dictionary) {
-        insertFromCommandline(dictionary); // nhập dữ liệu từ vựng
-        showAllWords(dictionary); // in danh sách từ vựng theo alphabet
-    }
-    public void dictionarySearcher(String input) {
-        ArrayList<String> wordList = getWordList(); // Lấy danh sách word từ từ điển
-        ArrayList<String> resultList = new ArrayList<>(); // Tạo ds rỗng để chứa từ nối
-        for (String word : wordList) { // Vòng lặp từng từ trong danh sách từ
-            if (word.startsWith(input)) { // Kiểm tra xem từ đó có bắt đầu bằng từ nhập vào không
-                resultList.add(word); //Thêm từ vào danh sách kết quả
-            }
-        }
-        // Print the result list In ra ds kết quả
-        System.out.println("Các từ cần tìm " + input + " : ");
-        for (String word : resultList) {
-            System.out.println(word);
-        }
-    }
+    // public void insertFromCommandline(Dictionary dictionary) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Nhập số lượng từ vựng muốn thêm: ");
+    //     int n = sc.nextInt();
+    //     sc.nextLine();
+    //     for(int i = 0; i < n ;i ++) {
+    //         System.out.println("Nhập từ vựng tiếng anh: ");
+    //         String english = sc.nextLine();
+    //         System.out.println("Nhập nghĩa tiếng việt: ");
+    //         String vietnamese = sc.nextLine();
+    //         Word word = new Word(english, vietnamese); //tạo đối tượng word
+    //         dictionary.addWord(word); // thêm word vào dictionary
+    //     }
+    // }
+    // public void dictionaryBasic(Dictionary dictionary) {
+    //     insertFromCommandline(dictionary); // nhập dữ liệu từ vựng
+    //     showAllWords(dictionary); // in danh sách từ vựng theo alphabet
+    // }
+    // public void dictionarySearcher(String input) {
+    //     ArrayList<String> wordList = getWordList(); // Lấy danh sách word từ từ điển
+    //     ArrayList<String> resultList = new ArrayList<>(); // Tạo ds rỗng để chứa từ nối
+    //     for (String word : wordList) { // Vòng lặp từng từ trong danh sách từ
+    //         if (word.startsWith(input)) { // Kiểm tra xem từ đó có bắt đầu bằng từ nhập vào không
+    //             resultList.add(word); //Thêm từ vào danh sách kết quả
+    //         }
+    //     }
+    //     // Print the result list In ra ds kết quả
+    //     System.out.println("Các từ cần tìm " + input + " : ");
+    //     for (String word : resultList) {
+    //         System.out.println(word);
+    //     }
+    // }
 
-    public void dictionaryAdvanced() {
+    public void dictionaryAdvanced(Dictionary dictionary) throws IOException {
+        DictionaryManagement dictionaryManagement = new DictionaryManagement();
         Scanner sc = new Scanner(System.in);
         boolean exit = false; 
         while (!exit) { 
@@ -75,23 +78,23 @@ public class DictionaryCommandline {
                 exit = true;
                 System.out.println("Cảm ơn đã dùng ứng dụng từ điển");
             } else if (action == 1) { 
-                addWord();
+                dictionaryManagement.addWord(dictionary);
             } else if (action == 2) { 
-                removeWord();
+                dictionaryManagement.removeWord(dictionary);
             } else if (action == 3) { 
-                updateWord();
+                dictionaryManagement.updateWord(dictionary);
             } else if (action == 4) { 
-                displayWordList();
+                // displayWordList();
             } else if (action == 5) { 
-                lookupWord();
+                dictionaryManagement.dictionaryLookup(dictionary);
             } else if (action == 6) { 
-                searchWord();
+                // searchWord();
             } else if (action == 7) { 
-                playGame();
+                // playGame();
             } else if (action == 8) { 
-                importFromFile();
+                dictionaryManagement.insertFromFile(dictionary);
             } else if (action == 9) { 
-                exportToFile();
+                dictionaryManagement.dictionaryExportToFile(dictionary);
             } else { 
                 System.out.println("Action not supported");
             }
