8a6a752bd039e1481330c0fc1c71bd120e34ea92
Vo Trong Dan <trongdantvh@gmail.com>
2023-11-19 11:15:52 +0700

    update function main

--- dictionary/src/main/java/dictionary/App.java
+++ dictionary/src/main/java/dictionary/App.java
@@ -1,12 +1,19 @@
 package dictionary;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException {
+        Scanner input = new Scanner(System.in);
         Dictionary dictionary = new Dictionary();
-        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
-        dictionaryCommandline.dictionaryAdvanced(dictionary);
+        // DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+        // dictionaryCommandline.dictionaryAdvanced(dictionary, input);
+        DictionaryManagement dictionaryManagement = new DictionaryManagement();
+        dictionaryManagement.importFromDatabase(dictionary);
+        dictionaryManagement.dictionaryLookup(dictionary, input);
+        input.close();
     }
+
 }
